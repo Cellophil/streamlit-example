1dcc62e95d3f3a25b2797713307d71aa2af1b293
Andreas Hösl <andreas.hoesl@cellophil.de>
2022-07-19 14:25:00 +0200

    network start

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import pypsa
+import numpy as np
 
 """
 # Welcome to Streamlit!
@@ -16,23 +18,56 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+n = pypsa.Network()
+st.text(n)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+menu = st.sidebar(st.button('Generators'))
+menu = st.sidebar(st.button('Lines'))
+menu = st.sidebar(st.button('bla'))
+menu = st.sidebar(st.button('Download Network'))
 
-    points_per_turn = total_points / num_turns
+if menu is None:
+    menu = 'Generators'
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+for i in range(10):
+    n.add('Generator', name='my_gen_'+str(i), p_nom = np.random.rand()*200+50, p_min_pu=np.random.rand()*0.5, p_max_pu = 1-np.random.rand()*0.1, carrier=['wind', 'PV', 'hydro', 'gas', 'coal'][np.random.randint(0,5)])
+
+st.text(menu)
+if menu == 'Generators':
+    st.dataframe(n.generators)
+    selected_gen = st.selectbox('Select generator to change', n.geneartors.index.tolist())
+
+    if selected_gen is not None:
+        gen = n.generators.loc[selected_gen, :]
+        st.number_input('p_nom', value=gen.p_nom)
+        st.number_input('p_min_pu', value=gen.p_nom)
+        st.number_input('p_max_pu', value=gen.p_nom)
+
+        st.button('Submit')
+    
+        
+
+
+
+if False:
+    with st.echo(code_location='below'):
+        total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+        num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+        Point = namedtuple('Point', 'x y')
+        data = []
+
+        points_per_turn = total_points / num_turns
+
+        for curr_point_num in range(total_points):
+            curr_turn, i = divmod(curr_point_num, points_per_turn)
+            angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+            radius = curr_point_num / total_points
+            x = radius * math.cos(angle)
+            y = radius * math.sin(angle)
+            data.append(Point(x, y))
+
+        st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+            .mark_circle(color='#0068c9', opacity=0.5)
+            .encode(x='x:Q', y='y:Q'))
